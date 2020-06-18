4f71925fc212bc05f90d9db8f83a69789e2a4978
Amaliya14 <isnaamaliya4@gamil.com>
2020-06-18 22:31:13 +0700

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,8 @@
+@extends('templates.app')
+
+@section('content')
+<section class="content-header">
+
+  </section>
+
+@endsection
---
+++ resources/views/templates/app.blade.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>SIG Perumahan</title>
+
+        <!-- Required CSS files -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i" rel="stylesheet">
+    <link rel="stylesheet" href="{{url('assets/css/owl.carousel.css')}}">
+    <link rel="stylesheet" href="{{url('assets/css/barfiller.css')}}">
+    <link rel="stylesheet" href="{{url('assets/css/animate.css')}}">
+    <link rel="stylesheet" href="{{url('assets/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{url('assets/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{url('assets/css/slicknav.css')}}">
+    <link rel="stylesheet" href="{{url('assets/css/main.css')}}">
+</head>
+
+<body>
+    <div class="preloader">
+        <span class="preloader-spin"></span>
+    </div>
+    <div class="site">
+        
+
+        
+        <header>
+            <div class="container">
+                <div class="row">
+                     <div class="col-xl-15">
+                          <section class="content-header">
+                             <div class="section-title white">
+                            <a class="text-center" href="index.html"><h2><b>Sistem Informasi Geografis Pemetaan Perumahan Di Kota Tegal</b></h2></a>
+                            </section>
+                        </div>
+                    <div class="col-6 col-sm-9 nav-column clearfix">
+                        <div class="right-nav">
+                            <span class="search-icon fa fa-search"></span>
+                            <form action="#" class="search-form">
+                                <input type="search" placeholder="search now">
+                                <button type="submit"><i class="fa fa-search"></i></button>
+                            </form>
+                        </div>
+                        <div class="col-xl-15">
+                        <nav id="menu" class="d-none d-lg-block">
+                            <ul>
+                                <li><a href="{{url('/home')}}">Home</a></li>
+                                <li><a href="{{url('peta')}}">Peta</a></li>
+                                <li><a href="{{url('perumahan')}}">Perumahan</a></li>
+                                <li><a href="{{url('kecamatan')}}">Kecamatan</a></li>
+                                <li class="has-child">
+                                    <a href="{{url('info')}}">Info</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="{{url('nama_pengembang')}}">Nama Pengembang</a></li>
+                                        <li><a href="{{url('detail-perum')}}">Detail Perumahan</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="contact.html">Contact</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </header>        
+
+        <div class="hero-slider">
+            <div class="single-slide" style="background-image: url('assets/perum/ndalem-parikesit.jpg')">
+                <div class="inner">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-6 text-center">
+                                <div class="slide-content">
+                                    <h2>Ndalem Parikesit</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="single-slide" style="background-image: url('assets/perum/nirmala-estate1.jpg')">
+                <div class="inner">
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-6 text-center">
+                                <div class="slide-content">
+                                    <h2>Nirmala Estate 1</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="single-slide" style="background-image: url('assets/perum/griya-taman-ruslani.JPG')">
+                <div class="inner">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="slide-content">
+                                    <h2>Griya Taman Ruslani</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+ <div class="service-area bg2 sp">
+<div class="portfolio-area sp">
+    <div class="container">
+        <div class="section-title">
+            <h2>Perumahan</h2>
+        </div>
+        <div class="row">
+            <div class="single-portfolio col-md-4">
+                <div class="inner">
+                    <div class="portfolio-img">
+                        <img src="{{url('assets/perum/tegal-residence1.jpg')}}" alt="portfolio-image">
+                        <div class="hover-content">
+                            <div>
+                                <a href="#" class="button">View Case Study</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="portfolio-content">
+                        <a href="#"><h3>Tegal Residence</h3></a>
+                    </div>
+                </div>
+            </div>
+            <div class="single-portfolio col-md-4">
+                <div class="inner">
+                    <div class="portfolio-img">
+                        <img src="{{url('assets/perum/palm-town-house.jpeg')}}" alt="portfolio-image">
+                        <div class="hover-content">
+                            <div>
+                                <a href="#" class="button">View Case Study</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="portfolio-content">
+                        <a href="#"><h3>Palm Town House</h3></a>
+                    </div>
+                </div>
+            </div>
+            <div class="single-portfolio col-md-4">
+                <div class="inner">
+                    <div class="portfolio-img">
+                        <img src="{{url('assets/perum/Edewlweiss.jpg')}}" alt="portfolio-image">
+                        <div class="hover-content">
+                            <div>
+                                <a href="#" class="button">View Case Study</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="portfolio-content">
+                        <a href="#"><h3>Cluster E'Dewlweiss</h3></a>
+                    </div>
+                </div>
+            </div>
+            <div class="single-portfolio col-md-4">
+                <div class="inner">
+                    <div class="portfolio-img">
+                        <img src="{{url('assets/perum/graha-basama1.jpg')}}" alt="portfolio-image">
+                        <div class="hover-content">
+                            <div>
+                                <a href="#" class="button">View Case Study</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="portfolio-content">
+                        <a href="#"><h3>Graha Basama</h3></a>
+                    </div>
+                </div>
+            </div>
+            <div class="single-portfolio col-md-4">
+                <div class="inner">
+                    <div class="portfolio-img">
+                        <img src="{{url('assets/perum/sapphire-estate.jpg')}}" alt="portfolio-image">
+                        <div class="hover-content">
+                            <div>
+                                <a href="#" class="button">View Case Study</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="portfolio-content">
+                        <a href="#"><h3>Sapphire Estate</h3></a>
+                    </div>
+                </div>
+            </div>
+            <div class="single-portfolio col-md-4">
+                <div class="inner">
+                    <div class="portfolio-img">
+                        <img src="{{url('assets/perum/griya-brawijaya.png')}}" alt="portfolio-image">
+                        <div class="hover-content">
+                            <div>
+                                <a href="#" class="button">View Case Study</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="portfolio-content">
+                        <a href="#"><h3>Griya Brawijaya</h3></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    <footer>
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-lg-3 footer_widget">
+                    <div class="inner">
+                        <h4>Address</h4>
+                        <h5>Flat Pro, Inc.</h5>
+                        <p>649 Folsom Ave, Suite 400 <br>San Francisco, CA 63105 <br>P: (123) 456-7890</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="copyright-txt">
+                        © 2020 Sistem Informasi Geografis Pemetaan Perumahan Kota Tegal.
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+              
+
+    <!--Required JS files-->
+<script src="{{url('assets/js/jquery-2.2.4.min.js')}}"></script>
+<script src="{{url('assets/js/vendor/popper.min.js')}}"></script>
+<script src="{{url('assets/js/vendor/bootstrap.min.js')}}"></script>
+<script src="{{url('assets/js/vendor/owl.carousel.min.js')}}"></script>
+<script src="{{url('assets/js/vendor/isotope.pkgd.min.js')}}"></script>
+<script src="{{url('assets/js/vendor/jquery.barfiller.js')}}"></script>
+<script src="{{url('assets/js/vendor/loopcounter.js')}}"></script>
+<script src="{{url('assets/js/vendor/slicknav.min.js')}}"></script>
+<script src="{{url('assets/js/active.js')}}"></script>
+
+</body>
+
+</html>
